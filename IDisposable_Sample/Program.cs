21a59e6a19df13ab05ed86a507d71eff6229db27
ice19942335 <ice19942335@gmail.com>
2020-06-01 12:29:42 +0300

    Add project files.

---
+++ IDisposable_Sample/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace IDisposable_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+        }
+
+        private static void Test()
+        {
+            Person person = null;
+
+            try
+            {
+                person = new Person();
+
+            }
+            finally
+            {
+                if (person != null)
+                {
+                    person.Dispose();
+                }
+            }
+        }
+    }
+
+    class Person : IDisposable
+    {
+        private bool _disposed = false;
+        public string Name { get; set; }
+
+        //Interface implementation
+        public void Dispose()
+        {
+            Dispose(true);
+
+            //Prevent finalization
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    //Free Managed Resources
+                }
+
+                //Free Unmanaged Resources
+                _disposed = true;
+            }
+        }
+
+        ~Person()
+        {
+            Dispose(false);
+        }
+    }
+
+    class Derived : Person
+    {
+        private bool IsDisposed = false;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (IsDisposed) return;
+            if (disposing)
+            {
+                //Free managed resources
+            }
+            IsDisposed = true;
+            // Call base class dispose method
+            base.Dispose(disposing);
+        }
+    }
+
+}
+ 
